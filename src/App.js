8fb757688318f86c1d7e6c0ac625215584b10aa0
Anish Duwal <duwalanise@gmail.com>
2019-12-29 14:34:17 +0545

    card animation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
+import CardComponent from './components/card';
+import './App.scss';
 
-function App() {
+import deckImage from './images/deck.png';
+import card from './images/AS.png';
+
+const App = () => {
+  const [cards, setCards] = useState([]);
+  const [end, setEnd] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="bj-board">
+      <div className="deck-provider">
+        <img src={deckImage} alt="" />
+      </div>
+      {cards.map((card, idx) => (
+        <CardComponent key={idx} shift={idx} end={end} />
+      ))}
+      <div className="deck-collector">
+        <img src={deckImage} alt="" />
+      </div>
+      <div className="bj-actions">
+        <button
+          onClick={() => {
+            setCards(cards.concat([1]));
+            setEnd(false);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Hit
+        </button>
+        <button
+          onClick={() => {
+            setEnd(true);
+            setTimeout(() => {
+              setCards([]);
+            }, 300);
+          }}
+        >
+          Clear
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/card.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
+
+import card from '../images/AS.png';
+
+const Card = ({ shift, end }) => {
+  const { xys } = useSpring({
+    from: { xys: [0, 0, 1] },
+    xys: end ? [-85, 0, 1] : [-50 + shift, 50, 1],
+  });
+  return (
+    <animated.div
+      className="deck-cards"
+      style={{
+        transform: xys.interpolate(
+          (x, y, s) => `translate(${x}vw, ${y}vh) scale(${s})`,
+        ),
+      }}
+    >
+      <img src={card} alt="" />
+    </animated.div>
+  );
+};
+
+export default Card;
